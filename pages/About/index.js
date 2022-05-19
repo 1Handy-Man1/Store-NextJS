b5ce02d4eb6a3225d97719b254795a4c2c17d049
TheHandMaster <69435603+Hshipp267@users.noreply.github.com>
2022-05-18 21:24:50 -0700

    Added about page and got the id.js for admin to work

---
+++ pages/About/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function About() {
+
+    return (<>
+    <Head>
+        <title>About me</title>
+    </Head>
+    <h1>me stuff</h1>
+    </>)
+}
--- pages/AdminHome/[id].js
+++ pages/AdminHome/[id].js
@@ -19,6 +19,6 @@ export async function getServerSideProps({params}){
     const req = await fetch(`http://localhost:3000/${params.id}.json`)
     const data = await req.json()
     return{
-        props:{AdminArray:data},
+        props:{cards:data},
     }
 }
