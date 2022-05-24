848ca5a51afb4402005013d86816d2e87d061cde
TheHandMaster <69435603+Hshipp267@users.noreply.github.com>
2022-05-24 15:20:45 -0700

    finish all step for auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
+import { UserProvider } from '@auth0/nextjs-auth0'
 
 function MyApp({ Component, pageProps }) {
-  return <>
-    <Navbar/>
-
-    <Component {...pageProps} />
-   </>
+  return (
+    <UserProvider>
+      <Navbar/>
+      <Component {...pageProps} />
+    </UserProvider> 
+  )
 }
 
 export default MyApp
---
+++ pages/api/auth/[...auth0].js
@@ -0,0 +1,4 @@
+import { handleAuth } from "@auth0/nextjs-auth0";
+// import { handleAuth } from "../../../.next/server/pages/api/auth/[...auth0]"; 
+
+export default handleAuth();
